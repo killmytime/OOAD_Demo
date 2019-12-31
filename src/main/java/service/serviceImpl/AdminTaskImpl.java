8e31213062a340e5cfbc9d3202f159a1f8b5ef8b
killmytime <35403592@qq.com>
2019-12-31 21:27:53 +0800

    草版做完了，由于不需要持久化，，很多地方有点无从下手，写逻辑写一半也挺恶心的，索性不写了....yuyu加油

--- src/main/java/service/serviceImpl/AdminTaskImpl.java
+++ src/main/java/service/serviceImpl/AdminTaskImpl.java
@@ -1,21 +1,86 @@
 package service.serviceImpl;
 
+import configuration.TaskStatus;
+import configuration.UserProfile;
 import entity.Task;
 import entity.TaskPool;
+import entity.User;
 import service.AdminTaskService;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
-public class AdminTaskImpl implements AdminTaskService {
-    public Task postTask(Task task) {
-        return null;
-    }
+import static configuration.Global.AdminPool;
+import static configuration.Global.userList;
 
-    public boolean deleteTask(UUID taskID) {
+public class AdminTaskImpl implements AdminTaskService {
+    public boolean postTask(Task task, User user) {
+        if (checkAdmin(user)) {
+            ArrayList<Task> tasks = AdminPool.getTasks();
+            task.setTaskID(UUID.randomUUID());
+            tasks.add(task);
+            AdminPool.setTasks(tasks);
+            new AdminTaskImpl().TaskReload(task, user);
+            return true;
+        }
         return false;
     }
 
-    public void TaskInitial(TaskPool taskPool) {
+    public boolean deleteTask(UUID taskID, User user) {
+        if (checkAdmin(user)) {
+            ArrayList<Task> tasks = AdminPool.getTasks();
+            for (Task task : tasks) {
+                if (taskID.equals(task.getTaskID())) {
+                    task.setTaskStatus(TaskStatus.DELETE);
+                    AdminPool.setTasks(tasks);
+                    new AdminTaskImpl().TaskReload(task,user);
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
+    }
 
+    //针对一个数据持久化之后可能的场景，将主任务更新到用户个人任务那里去同时不修改用户已经更改的数据，题目没有要求到修改任务，所以这里只考虑添加删除，粗糙的做个遍历好了
+    public void TaskReload(Task task, User user) {
+        if (checkAdmin(user)) {
+            if (task.getTaskStatus() == TaskStatus.DELETE)
+                for (User userItem : userList) {
+                    ArrayList<Task> taskArrayList = userItem.getTaskPool().getTasks();
+                    for (Task taskItem : taskArrayList) {
+                        if (taskItem.getTaskID().equals(task.getTaskID())) {
+                            if (taskItem.getTaskStatus() == TaskStatus.POST)
+                                taskItem.setTaskStatus(TaskStatus.DELETE);
+                            taskItem.setLimit(0);
+                            TaskPool taskPool = userItem.getTaskPool();
+                            taskPool.setTasks(taskArrayList);
+                            userItem.setTaskPool(taskPool);
+                            break;
+                        }
+                    }
+                }
+            else if (task.getTaskStatus() == TaskStatus.POST)
+                for (User userItem : userList) {
+                    ArrayList<Task> taskArrayList = userItem.getTaskPool().getTasks();
+                    boolean flag = true;
+                    for (Task taskItem : taskArrayList) {
+                        if (taskItem.getTaskID().equals(task.getTaskID())) {
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if (flag) {
+                        taskArrayList.add(task);
+                        TaskPool taskPool = userItem.getTaskPool();
+                        taskPool.setTasks(taskArrayList);
+                        userItem.setTaskPool(taskPool);
+                    }
+                }
+        }
+    }
+
+    private boolean checkAdmin(User user) {
+        return user.getAuth().equals(UserProfile.ADMIN) && user.getUsername().equals(UserProfile.ADMIN.getUsername()) && user.getPassword().equals(UserProfile.ADMIN.getPassword());
     }
 }
